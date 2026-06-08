a2cc64f6abf3fba4dfeb5b51b91941ff6cc5fc63
Senadyou <00wijaya@gmail.com>
2026-06-08 22:02:59 +0700

    Delete Frontend directory

--- Frontend/includes/user_info.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['NAMA_USER'])) {
-  echo "👋 Halo, " . htmlspecialchars($_SESSION['NAMA_USER']);
-} else {
-  echo "<span class='text-warning'>Belum login</span>";
-}
-?>
